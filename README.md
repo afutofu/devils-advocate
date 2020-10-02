24f0b8b01c07cdb539ea47169d694cf7bdab3797
Afuza <theafuza@gmail.com>
2020-10-02 14:53:41 +0700

    first commit, created express server and connected mongodb

---
+++ README.md
@@ -0,0 +1,3 @@
+# Devil's Advocate (MERN)
+
+This is an E-commerce website for One Piece's Devil Fruits
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// CONNECT TO DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => console.log("Connected to DB")
+);
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
